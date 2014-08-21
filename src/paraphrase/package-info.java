7a98018af2679e9e49ce62be561ab360ef40ac05
advecchia <simagicall@gmail.com>
2014-08-21 10:31:38 -0300

    Adding paraphrase package.

---
+++ src/paraphrase/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Automatic paraphrase identification based on Kim and Nakov work:
+ * "Large-Scale Noun Compound Interpretation Using Bootstrapping and the Web as a Corpus" 
+ */
+/**
+ * @author advecchia
+ *
+ */
+package paraphrase;
